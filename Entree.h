d92fc763ab1fba3eedd258d20320e2bf56c7094d
nathan-insa <nathan.arsac@insa-lyon.fr>
2016-04-02 22:04:54 +0200

    ajout squellettes aux .h de entree et mere

--- Entree.h
+++ Entree.h
@@ -1,12 +1,34 @@
+/*************************************************************************
+                           Entree  -  description
+                             -------------------
+    début                : 23/03/2016
+    copyright            : (C) 2016 par Nathan Arsac
+*************************************************************************/
+
+//---------- Interface de la tâche <Entree> (fichier Entree.h) -------
 #ifndef ENTREE_H
 #define ENTREE_H
 
+//------------------------------------------------------------------------
+// Rôle de la tâche <Entree>
+//	La tache Entrée permet de gérer les voituriers qui rentrent dans le
+//	Parking
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
 #include "Outils.h"
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
 
 void GestionEntree(int canalEntree[][2], int canalSortie[2], TypeBarriere typeEntree, int shmIdParking, int shmIdRequete, int semId);
-/* Procedure representant le fonctionnement de la tache GestionEntree
-contrat:
--TypeBarriere doit etre une valeur de voie parmi PROF_BLAISE_PASCAL, AUTRE_BLAISE_PASCAL, ENTREE_GASTON_BERGER
-*/
+//  Mode d'emploi :
+//	Processus fils Entree
+//  Contrat :
+//
 
 #endif
--- Mere.h
+++ Mere.h
@@ -1,6 +1,32 @@
+/*************************************************************************
+                           Mere  -  description
+                             -------------------
+    début                : 23/03/2016
+    copyright            : (C) 2016 par Nathan Arsac
+*************************************************************************/
+
+//---------- Interface de la tâche <Mere> (fichier Mere.h) -------
 #ifndef MERE_H
 #define MERE_H
 
+//------------------------------------------------------------------------
+// Rôle de la tâche <Mere>
+//	Le role de la tache mere est de coordonner la creation et la destruction
+//	de ses processus fils et de mettre en place et detruire les
+//	differentes ressources systemes crées
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
 int main(void);
+//  Mode d'emploi :
+//	Processus principal Mere
 
 #endif
